69b8c2f1de810c39fccb4c11e453c1ea3cd6cd3a
dognepiyush3d <dognepiyush3d@gmail.com>
2024-04-16 10:54:18 +0530

    my new responsive commits

--- src/App.jsx
+++ src/App.jsx
@@ -31,37 +31,42 @@ const App = () => {
   };
 
   return (
-    <div className='h-screen m-0 w-screen bg-zinc-500'>
-    <div className="mx-auto pt-10 relative w-1/3">
+    <div className='container h-screen bg-zinc-600 w-full'>
+    <div className="mx-auto pt-10 w-1/3 sm:w-2/3 xs:w-full xs:px-4 md1:w-full md2:w-full">
       <h1 className="text-2xl font-bold mb-4">Todo List</h1>
      
-     <div className='flex gap-4 w-full'>
+     <div className='flex lg:flex-row gap-4 w-full sm:flex-col xs:flex-col md:flex md:w-full'>
       <input
         type="text"
-        className="border border-gray-300 p-2 rounded-md w- mb-4 w-[24rem]"
+        className=" field border border-gray-300 p-2 rounded-md mb-4 3xl:w-full w-full focus:outline-none "
         placeholder="Add a new todo"
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleAddTodo();
+          }
+        }}
         />
       <button
-        className="buttons bg-blue-500 text-white px-4 py-2 rounded-md h-[2.7rem]"
+        className="buttons bg-blue-500 text-white px-4 py-2 rounded-md h-[2.7rem] sm:w-full md:w-[14rem]"
         onClick={handleAddTodo}
         >
         Add Todo
       </button>
     </div>
-      <ul className="mt-4">
+      <ul className="mt-4 xs:text-xs">
         {todos.map(todo => (
           <li
           key={todo.id}
-          className={`flex bg-slate-50 mt-5 bg-opacity-20 p-4 rounded-md items-center justify-between ${
+          className={`flex bg-slate-50  w-full mt-5 bg-opacity-20 p-4 xs:p-3 rounded-md items-center justify-between ${
             todo.completed ? 'line-through text-gray-500' : ''
           }`}
           >
-            <span>{todo.text}</span>
-            <div>
+            <div className='max-w-30 h-auto overflow-hidden text-wrap flex'>{todo.text}</div>
+            <div className='xs:flex'>
               <button
-                className="buttons mr-4 bg-slate-50 bg-opacity-5 text-white p-1 rounded-md"
+                className="buttons mr-4 bg-slate-50 bg-opacity-5 text-white p-1 rounded-md xs:h-10 xs:mt-0"
                 onClick={() => handleToggleTodo(todo.id)}
               >
                 {todo.completed ? 'Undo' : 'Complete'}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        xs:'320px',
+        md1:'425px',
+        md2:'375px'
+      }
+    },
   },
   plugins: [],
 }
